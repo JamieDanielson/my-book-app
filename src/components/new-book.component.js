b9e2539a9eb4d8fa64bc11f0e7904d8d76bdfb54
Jamie Danielson <jamieedanielson@gmail.com>
2020-07-18 19:19:03 -0400

    basic start

---
+++ src/components/new-book.component.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class NewBook extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeAuthor = this.onChangeAuthor.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      title: '',
+      author: '',
+    };
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+  onChangeAuthor(e) {
+    this.setState({
+      author: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const book = {
+      title: this.state.title,
+      author: this.state.author,
+    };
+    console.log(book);
+
+    axios
+      .post('http://localhost:5000/books/add', NewBook)
+      .then((res) => console.log(res.data));
+
+    window.location = '/';
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Add New Book</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Title: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.title}
+              onChange={this.onChangeTitle}
+            />
+          </div>
+          <div className="form-group">
+            <label>Author: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.author}
+              onChange={this.onChangeAuthor}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Add New Book"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
