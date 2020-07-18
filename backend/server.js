e8a26b19a6001bb28d5c334ec7bfcd523ff87215
Jamie Danielson <jamieedanielson@gmail.com>
2020-07-18 18:28:10 -0400

    add book model

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
+const booksRouter = require('./routes/books');
+app.use('/books', booksRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
